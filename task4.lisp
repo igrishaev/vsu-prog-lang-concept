c348d1dcb776566abb593ead0d3da6ff46f08c7b
Ivan Grishaev <ivan@grishaev.me>
2015-12-05 19:21:25 +0300

    tests added

--- task4.lisp
+++ task4.lisp
@@ -12,5 +12,28 @@
 (defun bin (num)
   (rebase num 2))
 
+(defun oct (num)
+  (rebase num 8))
+
 (defun hex (num)
   (rebase num 16))
+
+(defun maya (num)
+  (rebase num 20))
+
+(defun run-tests ()
+  (assert (string= (bin 1) "1"))
+  (assert (string= (bin 2) "10"))
+  (assert (string= (bin 10) "1010"))
+  (assert (string= (bin 11) "1011"))
+  (assert (string= (bin 255) "11111111"))
+  (assert (string= (oct 8) "10"))
+  (assert (string= (oct 1234) "2322"))
+  (assert (string= (hex 1) "1"))
+  (assert (string= (hex 10) "a"))
+  (assert (string= (hex 15) "f"))
+  (assert (string= (hex 255) "ff"))
+  (assert (string= (hex (* 256 256)) "10000"))
+  (assert (string= (maya 20) "10"))
+  (assert (string= (maya 19) "j"))
+  (format *standard-output* "Tests OK"))
